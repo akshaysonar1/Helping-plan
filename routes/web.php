ad7fa99c62aca824f6519deca11a04121f28377c
Neel <gupta.n@loquare.com>
2023-05-03 20:11:50 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\admin\GenrateUniqueCodeController;
-use App\Http\Controllers\HomeController;
+
 
 
 use App\Http\Controllers\admin\CustomerDetailsController;
@@ -11,7 +11,7 @@ use App\Http\Controllers\user\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DemoController;
 use App\Http\Controllers\Auth\LoginController;
- use Auth;
+//  use Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,12 +26,13 @@ use App\Http\Controllers\Auth\LoginController;
 
 
 
-Auth::routes();
+
   
     Route::get('/demo', [DemoController::class, 'demo']);
 
 
 
+    Auth::routes();
 Route::prefix('admin')->controller(GenrateUniqueCodeController::class)->group(function () {
     Route::get('/genratepin','generateUniqueCode')->name('genratepin');
     Route::post('/genratepin/data', 'storepin')->name('genratepin.data');
@@ -41,7 +42,7 @@ Route::prefix('admin')->controller(HomeController::class)->group(function () {
     Route::get('status/change/{user_Id}','UserStatus')->name('status');
 });
 
-});
+
 
 // Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 Route::middleware(['auth', 'isAdmin'])->group(function () {
