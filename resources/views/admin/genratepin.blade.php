fdd8f6dd666754d67d003b2c7c89f94bc9f67eae
akshaysonar1 <akshaysonar@sourcecube.in>
2023-05-09 16:16:38 +0530

    pin activation

--- resources/views/admin/genratepin.blade.php
+++ resources/views/admin/genratepin.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts.master')
 @section('content')
+<head>
+    <style>
+         
+        body, h1, h3, input { 
+        padding: 0;
+        margin: 0;
+        outline: none;
+        font-family: Roboto, Arial, sans-serif;
+        font-size: 16px;
+        color: #666;
+        }
+        
+       
+        form {
+        width: 86%; 
+        padding: 20px;
+        margin-bottom: 20px;
+        border-radius: 5px; 
+        border: solid 1px #ccc;
+        box-shadow: 1px 2px 5px rgba(0,0,0,.31); 
+        background: #ebebeb; 
+        }
+   button {
+        width: 100%;
+        padding: 8px;
+        border-radius: 5px; 
+        border: none;
+        background: #3b43d6; 
+        font-size: 14px;
+        font-weight: 600;
+        color: #fff;
+        }
+        button:hover {
+        background: #3b43d6;
+        }
+   
+        
+      </style>
+</head>
+
 <div class="container">
+    
     <div class="card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
@@ -8,9 +49,9 @@
                 <!-- <div class="col-lg-5 d-none d-lg-block bg-register-image"></div> -->
                 <div class="col-lg-7">
                     <div class="p-5">
-                        <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Genrate Pin </h1>
-                        </div>
+                       
+                        <h1 class="h3 mb-2 text-gray-800">Genrated Pin</h1>
+                 
                         <form class="form-group" method="post" action="{{route('genratepin.data')}}">
                             @csrf
                             Select Price For Pin
@@ -20,16 +61,23 @@
                                 <input id="name" type="radio" name="pin_ammount" value="2000" required autocomplete="name" autofocus> <span class="form-group">2000</span>
                             </div>
 
-                            <label>Enter The Pin </label>
+                            <label>Enter Total Pin </label><br>
+                            
+                                <input type="text" class="form-control"  name="total_pin" placeholder="Enter The pin ">
+                             
+                            
+                            <br>
+                            <button type="submit" >Submit</button>
                             <div class="col-sm-6">
-                                <input type="text" name="total_pin" required autocomplete="new-password" placeholder="Enter The pin ">
+                               
                             </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    
                     </form>
                 </div>
                 <div class="container-fluid">
-
+                    
+                        <div class="p-4">
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Genrated Pin</h1>
                     <!-- DataTales Example -->
@@ -63,7 +111,7 @@
                             </div>
                         </div>
                     </div>
-
+                        </div>
                 </div>
 
                 
